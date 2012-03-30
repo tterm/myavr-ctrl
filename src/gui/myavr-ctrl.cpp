b34447dd669d21114779d01daca66d2aa64bfe30
tterm <tterm@strace.org>
2012-03-30 19:07:14 +0200

    changed default style

--- src/gui/myavr-ctrl.cpp
+++ src/gui/myavr-ctrl.cpp
@@ -18,7 +18,6 @@ int main(int argc, char ** argv) {
 	LoggerPtr logger(Logger::getLogger("gui.main"));
 	DOMConfigurator::configure("../conf/log4cxx.xml");
 	LOG4CXX_INFO(logger, "Start myavr control gui");
-	QApplication::setStyle("plastique");
 	QApplication app(argc, argv);
 	backend::ConfigModel * model = new backend::ConfigModel;
 	MainWindowPresenter mwp(model);
